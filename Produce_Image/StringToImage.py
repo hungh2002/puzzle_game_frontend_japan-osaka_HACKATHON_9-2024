027fec453e6e92705c8772b6fdc9bc11b10b7027
nekomoti22 <g2453602@cc.kyoto-su.ac.jp>
2024-09-21 22:28:38 +0900

    背景画像の生成

--- Produce_Image/StringToImage.py
+++ Produce_Image/StringToImage.py
@@ -1,9 +1,12 @@
 import readline
 import math
 import os
-
+import matplotlib.pyplot as plt
+import random
+import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 from datetime import datetime
+from matplotlib.transforms import Affine2D
 """
 issue
 1.正方形にするためどうしても空白が多くなってしまう
@@ -44,8 +47,60 @@ font = ImageFont.truetype(font_path, 60)
 # RGB, 画像サイズ, 背景色を設定
 # image = Image.new("RGB", (quarter*60 + 20, quarter*60 + 20), (255, 255, 255))
 
+#背景画像を生成
+x = 0
+y = 0
+size = 0
+imgsize = 2048
+fig,ax = plt.subplots(figsize=(20.48,20.48))
+#背景を白に設定
+ax.set_facecolor("white")
+
+square_size = imgsize // 4
+
+shapes = ["o","v","s","8","p","*","h","D","X"]
+for h in range(2):
+    for i in range(4):
+        for j in range(4):
+            #位置の記録
+            # 正方形の中心位置を計算
+            x_center = (j + 0.5) * square_size
+            y_center = (i + 0.5) * square_size
+
+            # print(x_center,y_center)
+
+            # ランダムなオフセットを追加
+            x_offset = random.uniform(-0.1 * square_size, 0.1 * square_size)
+            y_offset = random.uniform(-0.1 * square_size, 0.1 * square_size)
+
+            # print(x_offset,y_offset)
+
+            x = x_center + x_offset
+            y = y_center + y_offset
+
+            # print(x,y)
+
+            size = random.uniform(100, 5000)  # マーカーサイズ(面積)
+            color = (random.random(), random.random(), random.random())  # ランダムな色
+            marker = random.choice(shapes) 
+            rotate = random.uniform(0, 180)  # マーカーの回転角度
+            t = Affine2D().rotate_deg(rotate) + ax.transData  # 回転行列
+            # マーカーをプロット
+            ax.scatter(x, y, s=size, c=[color], marker=marker, alpha=0.4, transform=t)
+
+#図を正方形に
+ax.set_aspect('equal')
+# 軸を非表示に
+plt.axis("off")
+
+
+#画像を表示
+# plt.show()
+# 画像を保存
+plt.savefig("Produce_Image/backgroundImgProto.jpg", bbox_inches="tight", pad_inches=0)
+
 # 背景画像を読み込む
-backgroundImage = Image.open("Produce_Image/backgroundImg2.jpg")
+backgroundImage = Image.open("Produce_Image/backgroundImgProto.jpg")
 
 #文字サイズに合わせて画像をトリミング
 text_size = quarter*60 + 20
