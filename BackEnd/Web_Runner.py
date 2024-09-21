452ddb4b6cb9de5e3197009a6b90b843735a2216
mstka <mstka2525@outlook.jp>
2024-09-21 12:58:07 +0900

    Produce_Imageを合体

--- BackEnd/Web_Runner.py
+++ BackEnd/Web_Runner.py
@@ -14,6 +14,11 @@ import os
 import hashlib
 import time
 import json
+from PIL import Image, ImageDraw, ImageFont
+from datetime import datetime
+import math
+
+#from ..Produce_Image import StringToImage as STI
 
 os.chdir('/'.join(__file__.split('/')[:-1]))
 
@@ -21,6 +26,91 @@ IP_addr = 'bomu.info'
 port_num = 60000
 post_dic = {}
 get_dic = {}
+
+
+#作成した画像を16分割する
+def ImgSplit(im):
+    height = (quarter*60 + 20) // 4
+    width = (quarter*60 + 20) // 4
+
+    buff = []
+    #縦の分割
+    for y1 in range(4):
+        #横の分割
+        for x1 in range(4):
+            #画像の切り取り
+            x2 = x1 * width
+            y2 = y1 * height
+            # print(x2,y2,width + x2,height + y2)
+            c = im.crop((x2, y2, width + x2, height + y2))
+            buff.append(c)
+    return buff
+
+
+
+class Image_Maker:
+    def __init__(self):
+        pass
+
+    def produce_img(self):
+
+        # 複数行の文字を出すのに、行ごとリストに入れてforで回す
+        texts = []
+        quiz = []
+        with open("Quiz_Text.txt","r") as f: #入力されたファイルを読み込む(デフォでNO1_58_...スタートになってる)
+            for w in f.read():  #ファイルの文章を読み込む
+                texts.append(w)
+
+        #それぞれの文字列を1つの文字列に変換する
+        print(texts)
+        ntexts = "".join(texts)
+        print(ntexts)
+        quiz.append(ntexts)
+        print(quiz)
+        # test = ["こ れ は、","文 字 列 か ら","作 ら れ た ","画 像 で す 。"]
+
+        #文字列を4分割して、それぞれのリストに入れる
+        quarter = math.ceil(len(ntexts)/4)
+        print(quarter)
+
+        nquiz = [
+            ntexts[0:quarter],
+            ntexts[quarter:quarter*2],
+            ntexts[quarter*2:quarter*3],
+            ntexts[quarter*3:]
+        ]
+        print(nquiz)
+
+        # PCローカルのフォントへのパスと、フォントサイズを指定
+        #font_path = "/Library/Fonts/BIZUDGothic-Bold.ttf"
+        #font = ImageFont.truetype(font_path, 60)
+        font = ImageFont.truetype('Arial.ttf', 60)
+
+        # RGB, 画像サイズ, 背景色を設定
+        image = Image.new("RGB", (quarter*60 + 20, quarter*60 + 20), (255, 255, 255))
+
+        draw = ImageDraw.Draw(image)
+        # 文字描画の初期位置（画像左上からx, yだけ離れた位置）
+        x = 10
+        y = 10
+
+        # 文字の描画
+        for i in range(4):
+            # 描画位置、描画する文字、文字色、フォントを指定
+            draw.text((x, y), nquiz[i], fill=(0, 0, 0), font=font)
+            y += (quarter*60 - 60) // 3
+
+        # ファイルに出力
+        image.save("base_image.png")
+
+        #os.makedirs("SplitImg", exist_ok=True)
+        #ファイルを開く
+        im = Image.open("base_image.png")
+        ims = ImgSplit(im)
+        for i, img in enumerate(ims):
+            img.save("img_" + str(int(i%self.width)) + str(int(i/self.width)) + ".png")
+
+IM = Image_Maker()
 
 ###############################################################
 
@@ -147,6 +237,13 @@ class Read_Data:
     def get_make_room(self, path_split, user_cookie, bf_cookie_data):
         content_type = 'application/json'
 
+        self.ranking = []
+        self.question = "りんごは何色ですか？"
+        self.choice = ["赤","黄","青","緑"]
+        self.answer = 0
+
+        IM.produce_img()
+
         html_data = '''{
     "result":"success"
 }'''
@@ -199,8 +296,8 @@ class Read_Data:
         content_type = 'application/json'
 
         html_data = '''{
-    "question":'''+question+''',
-    "answer":'''+json.dumps(choice)+''',
+    "question":'''+self.question+''',
+    "answer":'''+json.dumps(self.choice)+''',
     "start_time":{
         "hour":"何時",
         "minute":"何分"
